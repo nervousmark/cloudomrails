323c55431875802af01467f988e0effbb0407ebc
Marwan Rateb <mrateb@gmail.com>
2018-09-29 13:02:34 +0000

    added acts as votable to tweets and items

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,8 @@
 class Item < ApplicationRecord
   belongs_to :user
+  
+  validates :user, presence: true
+  
+  acts_as_votable
+  
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,4 +2,6 @@ class Tweet < ApplicationRecord
   belongs_to :user
   validates :user, presence: true
   validates :content, presence: true, length: { maximum: 144 }
+  acts_as_votable
+  
 end
